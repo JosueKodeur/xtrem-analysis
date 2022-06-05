f5c66f2c7c497de68df44ea3397f8927531837c0
JosueKodeur <josuevoutikotor2019@gmail.com>
2022-06-05 11:55:58 +0000

    Commit Initial

---
+++ src/main/java/com/josue/kodeur/xtremanalyse/XtremAnalyseApplication.java
@@ -0,0 +1,13 @@
+package com.josue.kodeur.xtremanalyse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtremAnalyseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XtremAnalyseApplication.class, args);
+    }
+
+}
