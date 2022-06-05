369ace55b87bd68bd1fe61cf89b1fb17253e8075
JosueKodeur <josuevoutikotor2019@gmail.com>
2022-06-05 13:41:53 +0000

    Création des entités

---
+++ src/main/java/com/josue/kodeur/xtremanalyse/dtos/RegionDto.java
@@ -0,0 +1,7 @@
+package com.josue.kodeur.xtremanalyse.dtos;
+/**
+ * @author JosueKodeur
+ */
+ 
+public class RegionDto {
+}
---
+++ src/main/java/com/josue/kodeur/xtremanalyse/entities/Prefecture.java
@@ -0,0 +1,7 @@
+package com.josue.kodeur.xtremanalyse.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Prefecture {
+}
